0787ef6f2683f1ec2253c87deba0b96080263266
Jay S Kim <jay.kim@incedoinc.com>
2025-08-12 14:20:49 -0400

    AppDbContext Default User Created.

--- ECommerceAPI/Data/AppDbContext.cs
+++ ECommerceAPI/Data/AppDbContext.cs
@@ -4,14 +4,20 @@ using ECommerceAPI.Models;
 namespace ECommerceAPI.Data
 {
     public class AppDbContext : DbContext
-{
-
-    public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) : base(dbContextOptions)
     {
 
+        public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(
+                new User { Id = 1, Email = "admin@gmail.com", Password = "Passcode1", Role = Roles.Admin }
+                );
+        }
     }
-
-    public DbSet<User> Users { get; set; }
-
-}
 }
