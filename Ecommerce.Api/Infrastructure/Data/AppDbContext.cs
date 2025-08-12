a2d2783def7b0e0c7e9d936cc2d3c4cff0933055
Saakshi20 <saakshiparikh201201@gmail.com>
2025-08-12 14:33:34 -0400

    feat(auth): add user registration and login

---
+++ Ecommerce.Api/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Ecommerce.Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Api.Infrastructure.Data;
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public DbSet<User> Users => Set<User>();
+    protected override void OnModelCreating(ModelBuilder b)
+    {
+        b.Entity<User>().HasIndex(u => u.Email).IsUnique();
+        b.Entity<User>().Property(u => u.Email).IsRequired().HasMaxLength(200);
+        b.Entity<User>().Property(u => u.FullName).IsRequired().HasMaxLength(120);
+    }
+}
