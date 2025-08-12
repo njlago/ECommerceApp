3e7d9f3e13503113af23baf1ee28f474f28884bd
Nicholas Joseph Lago <nicholas.joseph@incedoinc.com>
2025-08-12 16:16:20 -0400

    consolidated files

--- ECommerceAPI/Program.cs
+++ ECommerceAPI/Program.cs
@@ -21,3 +21,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+int x = "hello";
--- ECommerceAPI/obj/Debug/net9.0/ECommerceAPI.AssemblyInfo.cs
+++ ECommerceAPI/obj/Debug/net9.0/ECommerceAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4b350821d91fa5456d5641f6808bae06e88b340")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eaab7bd9185f1cde70e9bae39a2ae4053dc192c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
