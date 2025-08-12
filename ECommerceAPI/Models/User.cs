0c4e4a7fa9385f2ba84d18a24c4d31613447ccc7
Saakshi20 <saakshiparikh201201@gmail.com>
2025-08-12 15:26:56 -0400

    User.cs

--- ECommerceAPI/Models/User.cs
+++ ECommerceAPI/Models/User.cs
@@ -1,14 +1,11 @@
-﻿using System;
+﻿namespace EcommerceAPI.Models;
 
-/// <summary>
-/// Summary description for Class1
-/// </summary>
-public class Class1
+public class User
 {
-	public Class1()
-	{
-		//
-		// TODO: Add constructor logic here
-		//
-	}
+    public int Id { get; set; }
+    public string FullName { get; set; } = default!;
+    public string Email { get; set; } = default!;
+    public string PasswordHash { get; set; } = default!;
+    public string Role { get; set; } = Roles.Customer;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
