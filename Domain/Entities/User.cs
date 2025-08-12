271f6e48e6132cf6d8a99cd85b3663e3621be15f
Saakshi20 <saakshiparikh201201@gmail.com>
2025-08-12 14:21:39 -0400

    feat(auth): bootstrap API + auth scaffolding

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Ecommerce.Api.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public string PasswordHash { get; set; } = default!;
+        public string Role { get; set; } = "Customer";
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
