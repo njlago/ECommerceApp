81676cb17d00863335d0087693ab520dc9122b09
Nicholas Joseph Lago <nicholas.joseph@incedoinc.com>
2025-08-12 12:06:50 -0400

    Authenticate Controller

---
+++ ECommerceAPI/Controllers/AuthenticateController.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[AllowAnonymous]
+
+public class AuthenticateController : Controller
+{
+    private IUserRepository userRepository;
+    private ITokenService tokenServiuce;
+
+    public AuthenticateController(IUserRepository userRepositoy, ITokenService tokenService)
+    {
+        this.userRepository = userRepository;
+        this.tokenServiuce = tokenService;
+    }
+
+    [HttpPost("login")]
+
+    public IActionResult Login([FromBody] Login user)
+    {
+        var userFound = userRepository.Login(user);
+        if (userFound != null)
+        {
+            var token = tokenService.GenerateToken(userFound);
+            return Ok(new { token });
+
+        }
+        else
+        {
+            return Unauthorized("Invalid credentials");
+        }
+    }
+
+    [HttpPost("register")]
+    public IActionResult Register([FromBody] User user)
+    {
+        if (ModelState.IsValid)
+        {
+            userRepository.Register(user);
+            return Ok("User registered successfully");
+        }
+        else
+        {
+            return BadRequest("User Info Invalid");
+        }
+    }
+
+}
+
