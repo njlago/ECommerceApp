39745364f7da1b30e2c130f781ff24332d87342f
Saakshi20 <saakshiparikh201201@gmail.com>
2025-08-13 10:32:07 -0400

    Add NotFoundException class

--- ECommerceAPI/obj/Debug/net9.0/ECommerceAPI.AssemblyInfo.cs
+++ ECommerceAPI/obj/Debug/net9.0/ECommerceAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27f284f6d8f4b467144c30b7b9b3bf41ea871342")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7549471a16fe8922b542955065a0f1674e1bf797")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
