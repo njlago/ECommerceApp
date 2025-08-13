2ee314b29612f539b6d916978e0d9525fe839014
Jay S Kim <jay.kim@incedoinc.com>
2025-08-13 12:20:26 -0400

    Product and Category models

---
+++ ECommerceAPI/Models/Category.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+namespace ECommerceAPI.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
