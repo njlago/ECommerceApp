ffa224b72b283f67e0a214c8d01adb6db98287c5
Saakshi20 <saakshiparikh201201@gmail.com>
2025-08-13 10:56:49 -0400

    Add BadRequestException and UnauthorizedException classes

---
+++ ECommerceAPI/Infrastructure/BadRequestException.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ECommerceAPI.Infrastructure
+{
+    public class BadRequestException : Exception
+    {
+        public BadRequestException() : base()
+        {
+        }
+
+        public BadRequestException(string message) : base(message)
+        {
+        }
+
+        public BadRequestException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
