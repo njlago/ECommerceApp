36524f679cc036b8d8af2b71743fb6249389dfd1
Jay S Kim <jay.kim@incedoinc.com>
2025-08-15 09:35:23 -0400

    Finished Unit Tests for ProductsController

--- ECommerceAPI.Nunit.Tests/ProductRepositoryTests.cs
+++ ECommerceAPI.Nunit.Tests/ProductRepositoryTests.cs
@@ -2,7 +2,6 @@
 using ECommerceAPI.Models;
 using Microsoft.EntityFrameworkCore;
 using Moq;
-using NUnit.Framework;
 namespace ECommerceAPI.Nunit.Tests
 {
     public class Tests
@@ -10,6 +9,7 @@ namespace ECommerceAPI.Nunit.Tests
         [SetUp]
         public void Setup()
         {
+        
         }
 
         [Test]
@@ -20,7 +20,34 @@ namespace ECommerceAPI.Nunit.Tests
         }
 
         [Test]
-        public void GetProducts()
+        public void GetProductsTest()
+        {
+            var data = new List<Product>
+            {
+                new Product { Id = 1, Name = "AAA", CategoryId = 3, Description = "aaa", Price = 1, Stock = 1 },
+                new Product { Id = 2, Name = "BBB" },
+
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<Product>>();
+            mockSet.As<IQueryable<Product>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<Product>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<Product>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<Product>>().Setup(m => m.GetEnumerator()).Returns(() => data.GetEnumerator());
+
+            var mockContext = new Mock<AppDbContext>();
+            mockContext.Setup(c => c.Products).Returns(mockSet.Object);
+
+            var service = new ProductRepository(mockContext.Object);
+            var blogs = service.GetProducts();
+
+            Assert.That(2, Is.EqualTo(blogs.Count));
+            Assert.That("AAA", Is.EqualTo(blogs[0].Name));
+            Assert.That("BBB", Is.EqualTo(blogs[1].Name));
+        }
+
+        [Test]
+        public void DeleteProductsTest()
         {
             var data = new List<Product>
             {
